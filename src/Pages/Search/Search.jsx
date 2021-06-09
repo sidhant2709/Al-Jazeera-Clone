a3972d64178f52683434119bdb4f1a46985634b6
sidhant <sidhant1593@gmail.com>
2021-06-09 05:55:49 +0530

    updated

--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -5,7 +5,6 @@
 // import getNewsData from './Utilities/Utilities';
 // import Paginate from './Paginate';
 
-
 // const Search = () => {
 //     const { title } = useParams();
 //     const [page, setPage] = useState(0);
@@ -26,7 +25,7 @@
 //             })
 //     }
 //     useEffect(() => {
-//         getData();        
+//         getData();
 //     }, [title])
 
 //     useEffect(() => {
@@ -37,7 +36,7 @@
 //     console.log(response);
 //     console.log(newsData);
 //     return (
-//         <>  
+//         <>
 //             <div className={styles.cards}>
 //                 <div className={styles.title}><h4>Search Result for {title}</h4></div>
 //                 {
@@ -59,10 +58,9 @@
 //             </div>
 //         </>
 //         )
-// }   
+// }
 
 // export default Search;
-
 
 // import React,{useState,useEffect} from 'react';
 // import { useParams } from 'react-router';
@@ -70,7 +68,6 @@
 // import styles from './Styles/Search.module.css';
 // import getNewsData from './Utilities/Utilities';
 // import Paginate from './Paginate';
-
 
 // const Search = () => {
 //     const { title } = useParams();
@@ -91,7 +88,7 @@
 //             })
 //     }
 //     useEffect(() => {
-//         getData();        
+//         getData();
 //     }, [title])
 
 //     useEffect(() => {
@@ -102,7 +99,7 @@
 //     console.log(response);
 //     console.log(newsData);
 //     return (
-//         <>  
+//         <>
 //             <div className={styles.cards}>
 //                 <div className={styles.title}><h4>Search Result for {title}</h4></div>
 //                 {
@@ -124,108 +121,116 @@
 //             </div>
 //         </>
 //         )
-// }   
+// }
 
 // export default Search;
 
-
-
-
-import React,{useState,useEffect} from 'react';
-import { useParams } from 'react-router';
-import SearchCard from './SearchCard';
-import styles from './Styles/Search.module.css';
-import getNewsData from './Utilities/Utilities';
-import Paginate from './Paginate';
-import Loader from 'react-loader-spinner';
-import { FooterContainer } from '../../Common/Footer/Containers/Footer';
-
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import SearchCard from "./SearchCard";
+import styles from "./Styles/Search.module.css";
+import getNewsData from "./Utilities/Utilities";
+import Paginate from "./Paginate";
+import Loader from "react-loader-spinner";
+import { FooterContainer } from "../../Common/Footer/Containers/Footer";
 
 const Search = () => {
-    const { title } = useParams();
-    const [page, setPage] = useState(0);
-    const [newsData, setNewsData] = useState([]);
-    const [response, setResponse] = useState([]);
-    const [isLoading, setLoading] = useState(false)
+  const { title } = useParams();
+  const [page, setPage] = useState(0);
+  const [newsData, setNewsData] = useState([]);
+  const [response, setResponse] = useState([]);
+  const [isLoading, setLoading] = useState(false);
 
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = response.length - 1;
+      }
+      return prevPage;
+    });
+  };
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > response.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
+  const handlePage = (index) => {
+    setPage(index);
+  };
+  const getData = () => {
+    setLoading(true);
+    getNewsData(title).then((res) => {
+      console.log(res.data);
+      let dataNewsApi = Paginate(res.data.articles);
+      console.log(dataNewsApi);
+      setLoading(false);
+      setNewsData(dataNewsApi[0]);
+      setResponse(dataNewsApi);
+    });
+  };
+  useEffect(() => {
+    getData();
+  }, [title]);
 
-    const prevPage = () => {
-        setPage((oldPage) => {
-            let prevPage = oldPage - 1;
-            if (prevPage < 0) {
-                prevPage = response.length-1;
-            }
-            return prevPage
-        })
-    }
-    const nextPage = () => {
-        setPage((oldPage) => {
-            let nextPage = oldPage + 1;
-            if (nextPage > response.length - 1) {
-                nextPage = 0;
-            }
-            return nextPage
-        })
-    }
-    const handlePage = (index) => {
-        setPage(index);
-    }
-    const getData = () => {
-        setLoading(true)
-        getNewsData(title)
-            .then((res) => {
-                console.log(res.data);
-                let dataNewsApi = Paginate(res.data.articles);
-                console.log(dataNewsApi);
-                setLoading(false)
-                setNewsData(dataNewsApi[0]);
-                setResponse(dataNewsApi);
-            })
-    }
-    useEffect(() => {
-        getData();        
-    }, [title])
+  useEffect(() => {
+    let pagedata = response[page];
+    setNewsData(pagedata);
+  }, [page]);
 
-    useEffect(() => {
-        let pagedata = response[page];
-        setNewsData(pagedata)
-    },[page])
-
-    console.log(response);
-    console.log(newsData);
-    return (
-        <>  
-        {isLoading && <Loader type="Grid"
-        color="#eb7e20"
-        height={600}
-        width={300}
-        className = {styles.loader__show}
-        timeout={3000} />}
-            {!isLoading && <div className={styles.cards}>
-                <div className={styles.title}><p>Search Result for:</p><span>{title.toUpperCase()}</span></div>
-                {
-                    newsData?.map((item, i) => {
-                        return (
-                            <SearchCard key={i} {...item}/>
-                        )
-                    })
-                }
-            </div>}
-            {!isLoading &&  <div className={styles.btnContainer}>
-                <button className={styles.prevBtn} onClick={prevPage}>&#60; Previous</button>
-                {
-                    response.map((item,index) => {
-                        return (
-                            <button key={index} className={index === page ? styles.activeBtn : styles.pageBtn} onClick={()=>handlePage(index)}>{index+1}</button>
-                        )
-                    })
-                }
-                <button className={styles.nextBtn} onClick={nextPage}>Next &#62;</button>
-            </div>}
-            <FooterContainer/>
-        </>
-        )
-}   
+  console.log(response);
+  console.log(newsData);
+  return (
+    <>
+      {isLoading && (
+        <Loader
+          type="Grid"
+          color="#eb7e20"
+          height={600}
+          width={300}
+          className={styles.loader__show}
+          timeout={3000}
+        />
+      )}
+      {!isLoading && (
+        <div className={styles.cards}>
+          <div className={styles.title}>
+            <p>Search Result for:</p>
+            <span>{title.toUpperCase()}</span>
+          </div>
+          {newsData?.map((item, i) => {
+            return <SearchCard key={i} {...item} />;
+          })}
+        </div>
+      )}
+      {!isLoading && (
+        <div className={styles.btnContainer}>
+          <button className={styles.prevBtn} onClick={prevPage}>
+            &#60; Previous
+          </button>
+          {response.map((item, index) => {
+            return (
+              <button
+                key={index}
+                className={index === page ? styles.activeBtn : styles.pageBtn}
+                onClick={() => handlePage(index)}
+              >
+                {index + 1}
+              </button>
+            );
+          })}
+          <button className={styles.nextBtn} onClick={nextPage}>
+            Next &#62;
+          </button>
+        </div>
+      )}
+      <FooterContainer />
+    </>
+  );
+};
 
 export default Search;
-
--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom"
+import {HashRouter} from "react-router-dom"
 import { Provider } from "react-redux"
 import { store } from './Redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
     <Provider store = {store}>
     <App />
     </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
